061459934b0e18ee2ad7430f270e55886a477ebd
Emin <neziriemin1@gmail.com>
2019-02-19 15:40:08 +0200

    Modify the sensor

--- distance_sensor.py
+++ distance_sensor.py
@@ -1,5 +1,6 @@
 from machine import I2C, Pin, DAC, PWM
 from hcsr04 import HCSR04
+from time import sleep_ms
 
 sensor = HCSR04(trigger_pin=16, echo_pin=0)
 
@@ -7,3 +8,4 @@ def startReading():
     while True:
         distance = sensor.distance_cm()  # get the distance
         print('Distance:', distance, 'cm')
+        sleep_ms(1000)
