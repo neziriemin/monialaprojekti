3d7865573bd4daaa970c5be002023cefbadaa686
Emin <neziriemin1@gmail.com>
2019-02-26 16:41:22 +0200

    Close the socket after each request

--- send_readings.py
+++ send_readings.py
@@ -1,10 +1,11 @@
 import urequests
 
 def send(distance):
-    distance = str(distance)
-    url = "https://eeromikkonen.com/sensor&distance=" +distance
-    print(url)
     try:
+        distance = str(distance)
+        url = "https://eeromikkonen.com/sensor&distance=" +distance
+        print(url)
         request = urequests.get(url)
+        request.close()
     except Exception as e:
         print("Request failed: "+ str(e))
