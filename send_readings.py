a4a2ce764b1fba16b9f12a9d5909ee3cd7ea6e22
Emin <neziriemin1@gmail.com>
2019-02-26 17:34:28 +0200

    Change url

--- send_readings.py
+++ send_readings.py
@@ -1,10 +1,10 @@
 import urequests
 
 def send(distance):
+    distance = str(distance)
+    url = "https://eeromikkonen.com/sensor/distance.php?distance=" +distance
+    print(url)
     try:
-        distance = str(distance)
-        url = "https://eeromikkonen.com/sensor&distance=" +distance
-        print(url)
         request = urequests.get(url)
         request.close()
     except Exception as e:
