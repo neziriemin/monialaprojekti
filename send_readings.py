0cc67764bf76540aea751fa99f8cafa3b6b57eee
Emin <neziriemin1@gmail.com>
2019-03-07 14:41:12 +0200

    Read server response and sleep if uploading fails

--- send_readings.py
+++ send_readings.py
@@ -1,4 +1,6 @@
 import urequests
+import ujson
+from time import sleep_ms
 
 def send(distance, room):
     distance = str(distance)
@@ -7,6 +9,13 @@ def send(distance, room):
     print(url)
     try:
         request = urequests.get(url)
+        response = request.content
+        responseJSON = ujson.loads(response)
+        if (responseJSON["success"]):
+            print("Upload succesful")
+        else:
+            print("Upload failed")
+            sleep_ms(30000)
         request.close()
     except Exception as e:
         print("Request failed: "+ str(e))
